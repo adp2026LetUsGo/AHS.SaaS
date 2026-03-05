16f5174267989d76c98cdce6d2121486f1fdc772
Armando Diaz <anconhillservices@outlook.com>
2026-03-05 02:28:30 +0100

    fix: resolve NotSupportedException in Gateway API by injecting AotJsonContext into TypeInfoResolverChain

--- src/Common/AHS.Common/Serialization/AotJsonContext.cs
+++ src/Common/AHS.Common/Serialization/AotJsonContext.cs
@@ -1,9 +1,15 @@
 using System.Text.Json.Serialization;
 using AHS.Common.Models;
+using System.Collections.Generic;
+
 namespace AHS.Common.Serialization;
-[JsonSourceGenerationOptions(WriteIndented = false, PropertyNamingPolicy = JsonKnownNamingPolicy.CamelCase)]
+
+[JsonSourceGenerationOptions(WriteIndented = false, 
+    PropertyNamingPolicy = JsonKnownNamingPolicy.CamelCase,
+    DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull)]
 [JsonSerializable(typeof(PredictionResponse))]
-[JsonSerializable(typeof(PredictRiskRequest))]
 [JsonSerializable(typeof(Dictionary<string, float>))]
 [JsonSerializable(typeof(List<PredictionResponse>))]
-public partial class AotJsonContext : JsonSerializerContext { }
+public partial class AotJsonContext : JsonSerializerContext
+{
+}
